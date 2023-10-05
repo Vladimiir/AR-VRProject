156820fb5f523b424266a3d0ec134cdc4bf4c974
Vladimir <lamaxur@gmail.com>
2023-10-05 21:34:15 +0300

    created project

---
+++ AR_VR/AR_VR/AR_VRApp.swift
@@ -0,0 +1,17 @@
+//
+//  AR_VRApp.swift
+//  AR_VR
+//
+//  Created by Vladimir Stasenko on 05.10.2023.
+//
+
+import SwiftUI
+
+@main
+struct AR_VRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
